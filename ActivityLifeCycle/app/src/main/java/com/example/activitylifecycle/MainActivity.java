826d5e32855939f1e48dd54c96f3c0c549642e44
biantou99 <D1204518@o365.fcu.edu.tw>
2024-07-29 11:13:38 +0800

    change

--- ActivityLifeCycle/app/src/main/java/com/example/activitylifecycle/MainActivity.java
+++ ActivityLifeCycle/app/src/main/java/com/example/activitylifecycle/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.activitylifecycle;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +22,8 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+    public void callMap(View view){
+        Intent i = new Intent()
     }
 }
